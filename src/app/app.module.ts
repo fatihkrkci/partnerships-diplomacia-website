6ad15070bcc0c5a7cc414b43c91c668d3e8cc875
Fatih Kürekçi <fatihkr02@gmail.com>
2023-10-05 00:00:57 +0300

    designed and coded home-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
+import { QuoteComponent } from './components/quote/quote.component';
+import { VenueComponent } from './components/venue/venue.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ScheduleComponent,
+    QuoteComponent,
+    VenueComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
